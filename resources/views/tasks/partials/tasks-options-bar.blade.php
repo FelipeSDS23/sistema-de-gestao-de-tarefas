ff9ff469de54d426b1be777bd03cf9e43dcbbf88
FelipeSDS23 <felipe03san@gmail.com>
2025-02-13 12:13:02 -0300

    Filtro e ordenação concluidos

--- resources/views/tasks/partials/tasks-options-bar.blade.php
+++ resources/views/tasks/partials/tasks-options-bar.blade.php
@@ -42,10 +42,18 @@
                 </div>
 
                 {{-- Botão de Aplicar --}}
-                <div class="w-35 sm:w-auto flex justify-center sm:justify-start ">
+                <div class="w-35 sm:w-auto flex justify-center sm:justify-start">
                     <button type="submit" class="bg-blue-500 text-white text-xs sm:text-sm px-4 py-2 rounded-lg hover:bg-green-700 transition-colors w-35 sm:w-auto">
                         Aplicar
                     </button>
+                </div>
+
+                {{-- Botão de Limpar filtros --}}
+                <div class="w-35 sm:w-auto flex justify-center sm:justify-start">
+                    <a href="{{ route('tasks.index') }}" class="bg-gray-500 text-white text-xs sm:text-sm px-4 py-2 rounded-lg hover:bg-gray-700 transition-colors w-35 sm:w-auto flex items-center justify-center">
+                        Limpar
+                        <span class="material-symbols-outlined px-2">clear</span>
+                    </a>
                 </div>
 
             </div>
